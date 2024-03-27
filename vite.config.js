bc900a91e30f14d5df0a45395c7e935f8c027011
Kaptikov <igor.zabirow02@mail.ru>
2024-03-27 12:47:54 +0500

    add ViteImageOptimizer

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
 // vite.config.js
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 import { defineConfig } from 'vite'
 
 export default defineConfig(() => {
   return {
     base: '/intec-test-second/',
+
+    plugins: [
+      ViteImageOptimizer({
+        jpg: {
+          quality: 100,
+        },
+        jpeg: {
+          quality: 100,
+        },
+        png: {
+          quality: 100,
+        },
+        webp: {
+          lossless: true,
+        },
+      }),
+    ],
   }
 })
