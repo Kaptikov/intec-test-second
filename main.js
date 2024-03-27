7a4de350e75b1cc527655282f67c52c1663639f7
Kaptikov <igor.zabirow02@mail.ru>
2024-03-27 13:27:17 +0500

    add sass, base styles

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import './style.css'
+// import './style.css'
+import './src/assets/styles/main.scss'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
